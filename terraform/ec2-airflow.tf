a702e6fb620daf0012e5f67776e0de3455a9bd8a
이승준 <127376832+hhee4455@users.noreply.github.com>
2025-06-17 15:48:12 +0900

    Refactor : 초기세팅 코드 삭제

--- terraform/ec2-airflow.tf
+++ terraform/ec2-airflow.tf
@@ -8,24 +8,4 @@ resource "aws_instance" "airflow_server" {
   tags = {
     Name = "${var.project_tag}-airflow-server"
   }
-
-  user_data = <<-EOF
-    #!/bin/bash
-    set -eux
-
-    apt-get update -y
-
-    # 필수 패키지만 설치
-    apt-get install -y docker.io git curl
-
-    systemctl start docker
-    systemctl enable docker
-    usermod -aG docker ubuntu
-
-    # docker-compose 설치
-    curl -L "https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-  EOF
-
 }
