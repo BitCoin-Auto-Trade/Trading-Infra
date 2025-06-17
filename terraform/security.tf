430b65535c40e3a04310aa348f265bcd50fc58f0
이승준 <127376832+hhee4455@users.noreply.github.com>
2025-06-17 19:24:16 +0900

    Refactor : 보안그룹 설정 수정

--- terraform/security.tf
+++ terraform/security.tf
@@ -11,22 +11,6 @@ resource "aws_security_group" "db_sg" {
     cidr_blocks = [var.my_ip_cidr]
   }
 
-  ingress {
-    description = "PostgreSQL from Airflow"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    security_groups = [aws_security_group.airflow_sg.id]
-  }
-
-  ingress {
-    description = "Redis from Airflow"
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    security_groups = [aws_security_group.airflow_sg.id]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -62,4 +46,26 @@ resource "aws_security_group" "airflow_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# airflow → db(Postgres)
+resource "aws_security_group_rule" "db_postgres_from_airflow" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.db_sg.id
+  source_security_group_id = aws_security_group.airflow_sg.id
+  description              = "Postgres from Airflow SG"
+}
+
+# airflow → redis
+resource "aws_security_group_rule" "db_redis_from_airflow" {
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.db_sg.id
+  source_security_group_id = aws_security_group.airflow_sg.id
+  description              = "Redis from Airflow SG"
 }
